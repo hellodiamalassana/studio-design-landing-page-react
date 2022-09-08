0199803b800a947c675ef372a563db6377ebd077
leleking <simeonnortey@gmail.com>
2022-09-08 17:29:15 +0000

    added deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://Innovage-Labs.github.io/",
-  build: {
-    outDir: "./build",
-  },
   plugins: [react()],
 });
